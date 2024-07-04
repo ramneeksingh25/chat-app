095a43ecf1c3eddf4257b434a01633a3c77d7010
Ramneek Singh <singhrramneek@gmail.com>
2024-07-04 15:44:29 +0530

    add new friend UI

--- src/components/home/List/NewFriend.jsx
+++ src/components/home/List/NewFriend.jsx
@@ -1,23 +1,34 @@
 import React, { useState } from 'react'
 import { db } from '../../../config/firebase';
 import { collection, getDocs, query, where } from 'firebase/firestore';
+import Friend from '../components/Friend';
 
 const NewFriend = () => {
     const [errorMessage,setErrorMessage] =useState("Error");
     const [isSearching, setIsSearching] = useState(false);
-    const [friendName, setFriendName] = useState("");
+    const [friendEmail, setFriendEmail] = useState("");
+    const [FriendsList, setFriendsList] = useState([]);
+
     const usersRef = collection(db, "Users");
     const findFriend = async ()=>{
         setIsSearching(true);
         try {
             setErrorMessage("");
-            const friend= query(usersRef, where("displayName", "==", friendName));
+            setFriendsList([]);
+            const friend= query(usersRef, where("email", "==", friendEmail));
             const data = await getDocs(friend);
             console.log(data);
+            if(data.empty){
+                setErrorMessage("Can't find friend");
+                return;
+            }
+            const tempFriend = [];
             data.forEach((doc)=>{
-                console.log(doc);
+                tempFriend.push({id:doc.id,...doc.data()})
             })
-            console.log(data);
+            console.log(tempFriend);
+            setFriendsList(tempFriend);
+
         } catch (error) {
             console.log(error);
             setErrorMessage("Can't find friend");
@@ -26,15 +37,17 @@ const NewFriend = () => {
         }
     }
     const inputStyle =
-		"p-2 w-full text-gray-600 bg-white outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300 border-slate-300";
+		"p-2 m-0 w-full text-gray-600 bg-white outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300 border-slate-300";
   return (
 
-    <div className='w-96 text-xl text-indigo-800 dark:text-indigo-300 select-none space-y-5 p-6 shadow-xl rounded-xl bg-zinc-200/30 dark:bg-zinc-800/80 backdrop-blur-30'>
-        <h1 className='font-semibold'>Search for new friend</h1>
-        <label className="text-md font-bold underline decoration-pink-500/70 hover:decoration-indigo-700/70 transition duration-500">Name</label>
-        <input type="Name" className={inputStyle} onChange={(e)=>{
-            setFriendName(e.target.value);
-        }}/>
+    <div className='w-96 text-3xl text-indigo-800 dark:text-indigo-300 select-none space-y-2 p-6 shadow-xl rounded-xl bg-zinc-200/30 dark:bg-zinc-800/80 backdrop-blur-30'>
+        <h1 className='font-semibold'>Find Friends</h1>
+        <div className='flex flex-col gap-0'>
+            <label className="text-xl mb-0 font-bold underline decoration-pink-500/70 hover:decoration-indigo-700/70 transition duration-500">Email</label>
+            <input type="Name" className={inputStyle} onChange={(e)=>{
+                setFriendEmail(e.target.value);
+            }}/>
+        </div>
         <span className='text-red-500 font-light'>{errorMessage}</span>
         <button className={`w-full flex items-center justify-center gap-x-3 py-2.5 rounded-lg text-sm font-medium bg-gradient-to-r text-white  ${
 						isSearching
@@ -45,6 +58,9 @@ const NewFriend = () => {
                 findFriend();
             }}            
         >Add Friend</button>
+        {FriendsList.map((friend,index)=>{
+            return <Friend friend={friend} add={true} key={index}/>
+        })}
     </div>
   )
 }
--- src/components/home/components/Friend.jsx
+++ src/components/home/components/Friend.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
-import {  MdCall, MdOutlineMoreHoriz, MdVideocam } from 'react-icons/md'
+import React, { useState } from 'react'
+import {  MdArrowRight, MdBlock, MdCall, MdOutlineMoreHoriz, MdVideocam } from 'react-icons/md'
 import Avatar from './Avatar';
+import { IoPersonAddSharp } from 'react-icons/io5';
+import { RxCross1, RxCross2 } from 'react-icons/rx';
 
-const Friend = () => {
+const Friend = ({friend,add}) => {
+  const name=friend?.displayName;
+  const iconStyle='hover:bg-zinc-800 rounded-full cursor-pointer hover:text-zinc-200 dark:hover:text-zinc-200 transition-all duration-200 p-2 text-[170%]'
+  const photo = friend?.photo;
+  const [count,setCount]=useState(0);
   return (
-    <div className='mb-1 flex items-center justify-between border border-zinc-600 bg-zinc-300/80 hover:bg-zinc-400 text-zinc-900 dark:bg-zinc-700/80 dark:hover:bg-zinc-800 duration-300 p-3 transition-all dark:text-white cursor-pointer'>
+    <div className='mb-1 flex items-center justify-between border border-zinc-600 bg-zinc-300/80 hover:bg-zinc-400 text-zinc-900 dark:bg-zinc-700/80 dark:hover:bg-zinc-800 duration-300 p-3 transition-all dark:text-white cursor-pointer rounded-lg'>
         {/* <img src= alt='Ramneek' className='w-12 h-12 rounded-full'/> */}
-        <div className='flex items-center gap-2'>
-            <Avatar name="Ramneek Singh"/>
-            <h1>Ramneek Singh</h1>
+        <div className='flex items-center gap-2 mr-2'>
+            <Avatar name={name||"Ramneek Singh"}/>
+            <h1 className='hidden sm:hidden md:block lg:block'>{name||"Name"}</h1>
+        </div>
+        <div>
+
         </div>
         <div className='flex items-center text-2xl gap-4'>
-            <MdCall className='cursor-pointer  hover:text-zinc-600 dark:hover:text-zinc-400'/>
-            <MdVideocam className='cursor-pointer hover:text-zinc-600 dark:hover:text-zinc-400'/>
-            <MdOutlineMoreHoriz className='cursor-pointer hover:text-zinc-600 dark:hover:text-zinc-400'/>
+          
+        {!add?
+        <div className='hidden sm:hidden md:block lg:flex items-center justify-center gap-3'>
+            <MdCall className={iconStyle}/>
+            <MdVideocam className={iconStyle}/>
+        </div>
+        :<>
+          <IoPersonAddSharp className={iconStyle}/>
+        </>}
+        {
+          count===0?
+          <MdOutlineMoreHoriz className={iconStyle} onClick={()=>{setCount(1)}}/>:
+          <div className='flex items-center gap-1 transition-all duration-1000 '><MdBlock className={iconStyle} onClick={()=>{
+            //block user
+          }} />
+            <RxCross2 className={iconStyle+" text-red-500 hover:text-white hover:bg-red-500"} onClick={()=>{
+              setCount(0);
+            }}/>
+          </div>
+        }
         </div>
 
     </div>
