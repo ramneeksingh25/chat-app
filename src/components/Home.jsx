e7fb865139e94999febd3f338ac85b942eb2c198
Ramneek Singh <singhrramneek@gmail.com>
2024-07-11 20:52:37 +0530

    removed chatlist

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,89 +3,95 @@ import { auth, db } from "../config/firebase";
 import Loading from "./Loading";
 import List from "./home/List";
 import Chats from "./home/chat";
-import { collection, getDocs, onSnapshot, query, where } from "firebase/firestore";
+import {
+	collection,
+	getDocs,
+	onSnapshot,
+	query,
+	where,
+} from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setU } from "../redux/user/userSlice";
 export const userContext = createContext({
-  uid:"1234",
-  displayName:"John Doe",
-  email:"johndoe@example.com",
-  photoURL:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6SGvshARHJ5GYSH_Kig8-cYNw5rO3nWn7mA&s",
-  friends:["12345","12"],
-  requests:["http@example.com"],
-  sentReq:["abc@abc.com"]
+	uid: "1234",
+	displayName: "John Doe",
+	email: "johndoe@example.com",
+	photoURL:
+		"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6SGvshARHJ5GYSH_Kig8-cYNw5rO3nWn7mA&s",
+	friends: ["12345", "12"],
+	requests: ["http@example.com"],
+	sentReq: ["abc@abc.com"],
 });
 const Home = () => {
 	const dispatch = useDispatch();
-	const [email,setEmail]=useState(null);
-	const [id,setId]=useState(null);
+	const [email, setEmail] = useState(null);
+	const [id, setId] = useState(null);
 	const [loading, setLoading] = useState(true);
-	const [rerender,setRerender]=useState(false);
+	const [rerender, setRerender] = useState(false);
 	const navigate = useNavigate();
-	const [selected,setSelected] = useState(null);
+	const [selected, setSelected] = useState(null);
 	const fetchUserFromDB = async () => {
-		const q = query(collection(db,"Users"),where("email","==",auth.currentUser.email));
+		const q = query(
+			collection(db, "Users"),
+			where("email", "==", auth.currentUser.email)
+		);
 		const querySnapshot = await getDocs(q);
-		const data=querySnapshot.docs.map(doc=>doc.data());
+		const data = querySnapshot.docs.map((doc) => doc.data());
 		dispatch(setU(data[0]));
-	}
-	auth.onAuthStateChanged((user)=>{
+	};
+	auth.onAuthStateChanged((user) => {
 		if (!user) {
-			navigate("/")			
+			navigate("/");
 		}
-	})
-	useEffect(()=>{
+	});
+	useEffect(() => {
 		console.log("Home rendered");
 		setTimeout(() => {
 			setEmail(auth?.currentUser?.email);
 			setId(auth?.currentUser?.uid);
-			fetchUserFromDB()
-			
-		},1000);
-	},[rerender])
-	useEffect(()=>{
+			fetchUserFromDB();
+		}, 1000);
+	}, [rerender]);
+	useEffect(() => {
 		if (email) {
 			setLoading(false);
 		}
-	},[email])
-	useEffect(()=>{
-		if (selected==null) {
-			setProfileVisible(false);
+	}, [email]);
+	onSnapshot(
+		query(collection(db, "Users"), where("email", "==", email)),
+		(snapshot) => {
+			snapshot.docChanges().forEach((change) => {
+				if (change.type === "modified") {
+					console.log(change.doc);
+					setRerender(!rerender);
+				}
+			});
 		}
-	},[selected])
-	onSnapshot(query(collection(db,"Users"),where("email","==",email)),(snapshot)=>{
-		snapshot.docChanges().forEach((change)=>{
-			if (change.type === "modified") {
-				console.log(
-					change.doc
-				);
-                setRerender(!rerender);
-            }
-		})
-	})
-	const setUserChat = (s)=>{
-		console.log("selecting ",s);
+	);
+	const setUserChat = (s) => {
+		console.log("selecting ", s);
 		setSelected(s);
-	}
-	const [profileVisible, setProfileVisible] = useState(true);
-	const toggleProfile = (s)=>{
-		setProfileVisible(s);
-	}
+	};
 	const provider = {
-		id,email,selected,setUserChat,setRerender,profileVisible,toggleProfile
-	}
+		id,
+		email,
+		selected,
+		setUserChat,
+		setRerender,
+	};
 	return loading ? (
-    <div className="flex items-center justify-center h-screen text-[10vh]">
-      <Loading />
-    </div>
+		<div className="flex items-center justify-center h-screen text-[10vh]">
+			<Loading />
+		</div>
 	) : (
 		<>
 			<userContext.Provider value={provider}>
 				<div className=" h-screen w-[100%] grid grid-cols-4 gap-3 p-3 text-sm sm:text-sm md:text-md lg:text-lg text-indigo-900 dark:text-indigo-200 select-none">
 					<List />
-					<div className={`col-span-3 sm:col-span-3 md:col-span-3 lg:col-span-2 relative transition-all duration-500 ${profileVisible?" ":"lg:col-span-3"}`}>
-						<Chats/>
+					<div
+						className={`col-span-3 relative transition-all duration-500`}>
+						<Chats />
 					</div>
 				</div>
 			</userContext.Provider>
--- src/components/home/List/index.jsx
+++ src/components/home/List/index.jsx
@@ -1,36 +1,35 @@
 import React, { useContext, useEffect, useState } from "react";
 import ProfileInfo from "./FriendList/Profile";
-import AddFriend from "./FriendList/AddFriend";
 import RequestList from "./RequestsList";
 import FriendList from "./FriendList";
 import { useSelector } from "react-redux";
-import ChatList from "./ChatList";
 import { db } from "../../../config/firebase";
-import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { userContext } from "../../Home";
 
 const List = () => {
 	const User = useSelector((state) => state.user);
-	const {email}=useContext(userContext);
+	const { email } = useContext(userContext);
 	const friendsList = User.friends;
 	const requestList = User.requests;
-	const [chatList,setChats]=useState(null);
 	const menuStyle =
 		"p-1 rounded-xl rounded-b-none  cursor-pointer text-center flex-1";
-	const [viewFriends, setViewFriends] = useState(0);
-
-	const loadChats = async ()=>{
-        const q= query(collection(db,"Chats"),where("users","array-contains",email));
-        const querySnapshot = await getDocs(q);
-        const data=querySnapshot.docs.map(doc=>doc.data());
+	const [viewFriends, setViewFriends] = useState(1);
+	const loadChats = async () => {
+		const q = query(
+			collection(db, "Chats"),
+			where("users", "array-contains", email)
+		);
+		const querySnapshot = await getDocs(q);
+		const data = querySnapshot.docs.map((doc) => doc.data());
 		setChats(data);
-	}
-	useEffect(()=>{
+	};
+	useEffect(() => {
 		setTimeout(() => {
 			loadChats();
 		}, 2000);
 		console.log(email);
-	},[])
+	}, []);
 	return (
 		<div
 			className="h-[98vh] p-1 sm:p-1 md:p-2 lg:p-3 shadow-xl rounded-xl bg-zinc-200/10 dark:bg-zinc-800/40 backdrop-blur-30
@@ -40,18 +39,7 @@ const List = () => {
 			<div className="flex justify-between items-center ">
 				<span
 					className={`${menuStyle} ${
-						viewFriends==0
-							? " font-semibold bg-gradient-to-t from-zinc-200/10 to-zinc-200/20 dark:from-zinc-800/40 dark:to-zinc-800/50 "
-							: "hover:bg-zinc-200/5 dark:hover:bg-zinv-800/20 hover:h-[200%]"
-					} `}
-					onClick={() => {
-						setViewFriends(0);
-					}}>
-					Chats
-				</span>
-				<span
-					className={`${menuStyle} ${
-						viewFriends==1
+						viewFriends == 1
 							? " font-semibold bg-gradient-to-t from-zinc-200/10 to-zinc-200/20 dark:from-zinc-800/40 dark:to-zinc-800/50 "
 							: "hover:bg-zinc-200/5 dark:hover:bg-zinv-800/20 hover:h-[200%]"
 					} `}
@@ -62,7 +50,7 @@ const List = () => {
 				</span>
 				<span
 					className={`${menuStyle} ${
-						viewFriends==2
+						viewFriends == 2
 							? " font-semibold bg-gradient-to-t from-zinc-200/10 to-zinc-200/20 dark:from-zinc-800/40 dark:to-zinc-800/50"
 							: "hover:bg-zinc-200/5 dark:hover:bg-zinv-800/20 rounded-xl"
 					}`}
@@ -74,11 +62,6 @@ const List = () => {
 			</div>
 			<div
 				className={`bg-zinc-200/10 dark:bg-zinc-800/40 px-5 py-3 h-[80vh] rounded-xl rounded-t-none`}>
-				{viewFriends == 0 && (
-					<div>
-						<ChatList f={friendsList} />
-					</div>
-				)}
 				{viewFriends == 1 && (
 					<div>
 						<FriendList f={friendsList} />
