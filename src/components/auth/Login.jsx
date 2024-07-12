95ea7cafb7cb3764fd7714653585c5161b1a9bf0
Ramneek Singh <singhrramneek@gmail.com>
2024-07-12 17:58:09 +0530

    Auth Pages UI Changes

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,22 +1,19 @@
+import logo from '/favicon.svg'
 import {
 	getRedirectResult,
 	signInWithEmailAndPassword,
 	signInWithRedirect,
 } from "firebase/auth";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { auth, googleProvider } from "../../config/firebase";
-import { FcGoogle } from "react-icons/fc";
+import Input from "./Input";
 const Login = () => {
 	const [isSigningIn, setSigningIn] = useState(false);
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [errorMessage, setErrorMessage] = useState("");
-	const [response,setResponse]=useState("")
 	const navigate = useNavigate();
-	const inputStyle =
-		"w-full mt-2 px-3 py-2 text-gray-600 bg-white outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300 border-slate-300";
-	const boxStyle= "w-96 text-indigo-800 dark:text-indigo-300 select-none space-y-5 p-4 shadow-xl rounded-xl bg-zinc-200/30 dark:bg-zinc-800/80 backdrop-blur-30";
 	const signIn = async () => {
 		try {
 			setErrorMessage("");
@@ -28,69 +25,48 @@ const Login = () => {
 			setErrorMessage("Error Signing in! Check your credentials.");
 		}
 	};
-	const signInwithGoogle = async () => {
-		try {
-			console.log("GOOGLE");
-			signInWithRedirect(auth,googleProvider)
-			getRedirectResult(auth).then((result)=>{
-				console.log(result);
-			})
-			
-		} catch (error) {
-			console.error("Error signing in with Google:", error);
-			setErrorMessage("Error Signing in with Google");
-			setSigningIn(false);
-		}
-	};
 	auth.onAuthStateChanged((user) => {
 		if (user) {
 			setSigningIn(true);
 			navigate("/home");
 		}
 	});
+	
 	return (
-		<div className="flex items-center justify-center w-full h-screen">
-			<div className={boxStyle} onKeyDown={(e)=>{
-				if(e.key === "Enter"){
-                    signIn();
-                }
-			}}>
+		<div className="flex items-center justify-center w-full h-screen flex-col">
+			<div
+				className={
+					"w-96 text-indigo-800 dark:text-indigo-300 select-none space-y-5 p-7 shadow-xl rounded-lg bg-zinc-200/30 dark:bg-zinc-800/50 backdrop-blur-30"
+				}
+				onKeyDown={(e) => {
+					if (e.key === "Enter") {
+						signIn();
+					}
+				}}>
+			<div className='flex items-center justify-center'>
+				<img src={logo} alt="logo" className='w-16'/>
+				<div className="text-2xl flex-1 bg-gradient-to-r from-blue-500 to-pink-500 text-transparent bg-clip-text underline hover:from-blue-6 cursor-auto  hover:to-pink-600 select-none text-center">
+					<span className="text-5xl">ChatterBox</span> <br /> 
+					Chat, Share, Connect
+				</div>
+			</div>
 				<div className="text-center">
 					<div className="mt-2">
-						<h3 className="text-xl font-semibold sm:text-2xl select-none underline decoration-pink-500/70 hover:decoration-indigo-700/70 transition duration-1000">
-							Welcome Back!
+						<h3 className="select-none font-medium underline hover:decoration-pink-300/70 decoration-indigo-700/70 transition duration-1000">
+							Sign In to your account!
 						</h3>
 					</div>
 				</div>
-				<div>
-					<label className="text-sm font-bold underline decoration-pink-500/70 hover:decoration-indigo-700/70 transition duration-500">Email</label>
-					<input
-						type="email"
-						name="email"
-						autoComplete="email"
-						required
-						value={email}
-						onChange={(e) => {
-							setEmail(e.target.value);
-						}}
-						className={inputStyle}
-					/>
-				</div>
-				<div>
-					<label className="text-sm font-bold underline decoration-pink-500/70 hover:decoration-indigo-700/70 transition duration-500">Password</label>
-					<input
-						type="password"
-						name="password"
-						autoComplete="current-password"
-						required
-						value={password}
-						onChange={(e) => {
-							setPassword(e.target.value);
-						}}
-						className={inputStyle}
-					/>
-				</div>
-
+				<Input
+					type={"Email"}
+					onChange={setEmail}
+					required
+				/>
+				<Input
+					type={"Password"}
+					onChange={setPassword}
+					required
+				/>
 				{errorMessage && (
 					<span className="text-red-600 font-semibold">{errorMessage}</span>
 				)}
@@ -99,14 +75,14 @@ const Login = () => {
 					type="submit"
 					disabled={isSigningIn}
 					onClick={signIn}
-					className={`w-full px-4 py-2 text-white font-medium rounded-lg ${
+					className={`w-full px-4 py-2 text-white font-medium rounded-lg transition-all duration-500 bg-gradient-to-r from-indigo-600 to-pink-600 hover:from-indigo-700 hover:to-pink-700 hover:shadow-xl ${
 						isSigningIn
-							? "bg-gray-400 cursor-not-allowed"
+							? "bg-gray-300 cursor-not-allowed"
 							: " bg-gradient-to-r from-indigo-600 to-pink-600 hover:from-indigo-700 hover:to-pink-700 hover:shadow-xl transition duration-300"
 					}`}>
 					{isSigningIn ? "Signing In..." : "Sign In"}
 				</button>
-				<p className="text-center text-sm">
+				<p className="text-center text-sm text-indigo-700/90 dark:text-indigo-300/90 hover:text-pink-700/90 dark:hover:text-pink-300/90 transition duration-300">
 					{" "}
 					Dont have an account?{" "}
 					<Link
@@ -115,23 +91,6 @@ const Login = () => {
 						Sign up
 					</Link>
 				</p>
-				<div className="flex flex-row text-center w-full">
-					<div className="border-b-2 mb-2.5 mr-2 w-full border-slate-800/20 dark:border-slate-300/20"></div>
-					<div className="text-sm font-bold w-fit">OR</div>
-					<div className="border-b-2 mb-2.5 ml-2 w-full border-slate-800/20 dark:border-slate-300/20"></div>
-				</div>
-				<button
-					disabled={isSigningIn}
-					onClick={signInwithGoogle}
-					className={`w-full flex items-center justify-center gap-x-3 py-2.5 rounded-lg text-sm font-medium bg-gradient-to-r text-white  ${
-						isSigningIn
-							? "cursor-not-allowed from-slate-400/90 to-slate-500/70"
-							: "from-indigo-600/80 to-blue-700/50 hover:from-blue-700/80 hover:to-indigo-700/80 hover:shadow-xl transition duration-300"
-					}`}>
-					{isSigningIn ? "Signing In..." : "Continue with Google"}
-					<FcGoogle className="bg-slate-200 text-xl p-1 rounded-full"/>
-
-				</button>
 			</div>
 		</div>
 	);
--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,25 +1,25 @@
+import logo from "/favicon.svg";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { auth, db } from "../../config/firebase";
 import { doc, setDoc } from "firebase/firestore";
 import { MdOutlineAddPhotoAlternate } from "react-icons/md";
+import Input from "./Input";
 
 const Register = () => {
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [confirmPassword, setConfirmPassword] = useState("");
-	const [profilePic,setProfilePic] = useState({
-		file:null,
-		url:""
+	const [profilePic, setProfilePic] = useState({
+		file: null,
+		url: "",
 	});
 	const [isSigningIn, setSigningIn] = useState(false);
 	const [errorMessage, setErrorMessage] = useState("");
-	const inputStyle =
-		"w-full mt-2 px-3 py-2 text-gray-600 bg-white outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300 border-slate-300";
-	const boxStyle= "w-96 text-indigo-800 dark:text-indigo-300 select-none space-y-5 p-4 shadow-xl rounded-xl bg-zinc-200/30 dark:bg-zinc-800/50 backdrop-blur-30";
-	const textStyle = "text-sm font-bold underline decoration-pink-500/70 hover:decoration-indigo-700/70 transition duration-500"
+	const textStyle =
+		"text-sm font-bold underline decoration-pink-500/70 hover:decoration-indigo-700/70 transition duration-500";
 	const navigate = useNavigate();
 	const signUp = async () => {
 		if (password !== confirmPassword) {
@@ -32,27 +32,26 @@ const Register = () => {
 			await createUserWithEmailAndPassword(auth, email, password);
 			const user = auth.currentUser;
 			updateProfile(auth.currentUser, {
-				displayName: name
-			  })
+				displayName: name,
+			});
 			console.log(user);
 			if (user) {
 				await setDoc(doc(db, "Users", user.uid), {
-				  email: user.email,
-				  displayName: name,
-				  photoURL:profilePic.url,
-				  friends:[],
-				  requests: [],
-                  sentReq: [],
+					email: user.email,
+					displayName: name,
+					photoURL: profilePic.url,
+					friends: [],
+					requests: [],
+					sentReq: [],
 				});
-			  }
+			}
 
 			navigate("/home");
-
 		} catch (error) {
 			console.error("Error creating user:", error);
 			console.log(error);
-			setErrorMessage(error.message)
-			setSigningIn(false)
+			setErrorMessage(error.message);
+			setSigningIn(false);
 		}
 	};
 	auth.onAuthStateChanged((user) => {
@@ -63,39 +62,75 @@ const Register = () => {
 			}, 1000);
 		}
 	});
-	const handleAvatar=(e)=>{
-		if(e.target.files[0]){
-		setProfilePic({
-            file:e.target.files[0],
-            url:URL.createObjectURL(e.target.files[0])
-        })}
-	}
+	const handleAvatar = (e) => {
+		if (e.target.files[0]) {
+			setProfilePic({
+				file: e.target.files[0],
+				url: URL.createObjectURL(e.target.files[0]),
+			});
+		}
+	};
 	return (
 		<div>
 			<div className="flex items-center justify-center w-full h-screen select-none">
-				<div className={boxStyle} onKeyDown={(e)=>{
-				if(e.key === "Enter"){
-                    signUp();
-                }
-			}}>
+				<div
+					className={
+						"w-96 text-indigo-800 dark:text-indigo-300 select-none space-y-5 p-7 shadow-xl rounded-lg bg-zinc-200/30 dark:bg-zinc-800/50 backdrop-blur-30"
+					}
+					onKeyDown={(e) => {
+						if (e.key === "Enter") {
+							signUp();
+						}
+					}}>
+					<div className="flex items-center">
+						<img
+							src={logo}
+							alt=""
+							className="w-16"
+						/>
+						<div className="text-2xl flex-1 bg-gradient-to-r from-blue-500 to-pink-500 text-transparent bg-clip-text underline hover:from-blue-6 cursor-auto  hover:to-pink-600 select-none text-center">
+							<span className="text-5xl">ChatterBox</span> <br />
+							Chat, Share, Connect
+						</div>
+					</div>
 					<div className="text-center">
 						<div className="mt-2">
-							<h3 className={` text-xl font-semibold  ${textStyle} `}>
-								Please Enter your Details
+							<h3 className="select-none font-medium underline hover:decoration-pink-300/70 decoration-indigo-700/70 transition duration-1000">
+								Enter your details to register!
 							</h3>
 						</div>
 					</div>
 					<div className="flex items-center justify-center">
-						<label htmlFor="file" className="flex flex-col items-center gap-2 pointer-cursor">
-							{profilePic.file&&<img src={profilePic.url} className="w-[70px] h-[70px] border border-white/20 p-[1px] rounded-full hover:border-white/90 transition-all duration-300 cursor-pointer" alt="yourProfile" />}
-							<MdOutlineAddPhotoAlternate className={`bg-indigo-400 hover:bg-indigo-600 rounded-full transition-all duration-300 p-2 flex items-center justify-center text-white text-4xl cursor-pointer ${profilePic.file&&"hidden"}`}/>
+						<label
+							htmlFor="file"
+							className="flex flex-col items-center gap-2 pointer-cursor">
+							{profilePic.file && (
+								<img
+									src={profilePic.url}
+									className="w-[70px] h-[70px] border border-white/20 p-[1px] rounded-full hover:border-white/90 transition-all duration-300 cursor-pointer"
+									alt="yourProfile"
+								/>
+							)}
+							<MdOutlineAddPhotoAlternate
+								className={`bg-indigo-700 hover:bg-indigo-600
+								w-[50px] h-[50px] p-[10px]
+									rounded-full transition-all duration-500 flex items-center justify-center text-4xl cursor-pointer border border-pink-207hover:border-pink-400 text-pink-200 hover:text-pink-400 peer ${
+									profilePic.file && "hidden"
+								}`}
+							/>
 						</label>
-							{profilePic.file&&<div className="hover:underline hover:text-[102%] cursor-pointer" onClick={()=>{
-								setProfilePic({
-                                    file:null,
-                                    url:""
-                                })
-							}}>Remove</div>}
+						{profilePic.file && (
+							<div
+								className="hover:underline hover:text-[102%] cursor-pointer"
+								onClick={() => {
+									setProfilePic({
+										file: null,
+										url: "",
+									});
+								}}>
+								Remove
+							</div>
+						)}
 						<input
 							type="file"
 							id="file"
@@ -103,70 +138,32 @@ const Register = () => {
 							onChange={handleAvatar}
 						/>
 					</div>
-					<div>
-						<label className={textStyle}>
-							Enter Name
-						</label>
-						<input
-							type="text"
-							autoComplete="name"
-							required
-							value={name}
-							onChange={(e) => {
-								setName(e.target.value);
-							}}
-							className={inputStyle}
-						/>
-					</div>
-					<div>
-						<label className={textStyle}>
-							Enter Email
-						</label>
-						<input
-							type="text"
-							autoComplete="email"
-							required
-							value={email}
-							onChange={(e) => {
-								setEmail(e.target.value);
-							}}
-							className={inputStyle}
-						/>
-					</div>
-					<div>
-						<label className={textStyle}>
-							Enter Password
-						</label>
-						<input
-							type="password"
-							autoComplete="current-password"
-							required
-							value={password}
-							onChange={(e) => {
-								setPassword(e.target.value);
-							}}
-							className={inputStyle}
-						/>
-					</div>
-					<div>
-						<label className={textStyle}>
-							Confirm Password
-						</label>
-						<input
-							type="password"
-							autoComplete="current-password"
-							required
-							value={confirmPassword}
-							onChange={(e) => {
-								setConfirmPassword(e.target.value);
-							}}
-							className={inputStyle}
-						/>
-					</div><br />
+					<Input
+						type="Name"
+						onChange={setName}
+						required
+					/>
+					<Input
+						type="Email"
+						onChange={setEmail}
+						required
+					/>
+					<Input
+						type="Password"
+						onChange={setPassword}
+						required
+					/>
+					<Input
+						type="Confirm Password"
+						onChange={setConfirmPassword}
+						required
+					/>
 					{errorMessage && (
-						<span className="text-red-600 font-bold m-3 leading-7">{errorMessage}</span>
+						<span className="text-red-600 font-bold m-3 leading-7">
+							{errorMessage}
+						</span>
 					)}
-					
+
 					<button
 						type="submit"
 						disabled={isSigningIn}
@@ -175,10 +172,10 @@ const Register = () => {
 							isSigningIn
 								? "bg-gray-300 cursor-not-allowed"
 								: " bg-gradient-to-r from-indigo-600 to-pink-600 hover:from-indigo-700 hover:to-pink-700 hover:shadow-xl transition duration-300"
-								}`}>
+						}`}>
 						{isSigningIn ? "Signing In..." : "Register"}
 					</button>
-					<p className="text-center text-sm">
+					<p className="text-center text-sm text-indigo-700/90 dark:text-indigo-300/90 hover:text-pink-700/90 dark:hover:text-pink-300/90 transition duration-300">
 						{" "}
 						Already have an account?{" "}
 						<Link
